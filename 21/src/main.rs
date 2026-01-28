51ac1ea6051953a555d33941331b7b8add0a928b
bgieselmann <157547956+bgieselmann@users.noreply.github.com>
2026-01-28 14:26:50 -0800

    IT COMPILES NOW

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let hello_world_chars: [i32;12] = [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33];
+        let mut hello_world_str: [char;12] = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+        for n in 0..12 {
+            hello_world_str[n] = std::mem::transmute(hello_world_chars[n]);
+            println!("{}", hello_world_str[n]);
+        }
+    }
+
 }
+
+
